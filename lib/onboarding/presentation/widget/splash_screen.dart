a4c88c257227721050c05e726b971279943489be
n.sitnik <mad-4@mad.ru>
2025-02-26 14:40:49 +0700

    sprint2

---
+++ lib/onboarding/presentation/widget/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:project_prof4/onboarding/presentation/boarding_screens.dart/boarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+
+
+
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3),()=> Navigator.push(context, MaterialPageRoute(builder:(context) => BoardingScreen(),)));
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(72, 178, 231, 1),
+      body: Center(
+        // ignore: deprecated_member_use
+        child: Container(height: 120,width: 120,decoration:  const BoxDecoration(
+          shape: BoxShape.circle,
+          color:Colors.white
+        ),child: SvgPicture.asset('assets/bag-2.svg',height: 50,width: 50,color: const Color.fromRGBO(72, 178, 231, 1),)),
+      ),
+    );
+  }
+}
