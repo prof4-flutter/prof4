970c225ffcdd0afd107510251a752d0fafb68dfa
n.sitnik <mad-4@mad.ru>
2025-02-28 13:15:01 +0700

    Sprint-6

--- lib/main.dart
+++ lib/main.dart
@@ -16,5 +16,20 @@ void main(List<String> args) async{
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(url: 'https://jksykjgthwpyletmgihw.supabase.co', 
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imprc3lramd0aHdweWxldG1naWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3Njc3NTUsImV4cCI6MjA1NTM0Mzc1NX0.N1DOFH9SCvnQSkwa8fs3kIgsuBBZ9EZ_U7KPV_9ucWw');
-  runApp(MaterialApp(home: AuthGuard(),debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: NavigationBarS(index: 3,),debugShowCheckedModeBanner: false,));
+}
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(size: 240,
+        ),
+      ),
+    );
+  }
 }
